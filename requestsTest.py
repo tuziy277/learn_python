3575055dfed3ef87d9c665189fc54089404290e5
兔子y277 <277946261@qq.com>
2022-09-06 23:40:08 +0800

    test

--- requestsTest.py
+++ requestsTest.py
@@ -5,7 +5,7 @@ url = 'https://movie.douban.com/top250'
 header = {
    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
 }
-
+print('text')
 
 
 res = requests.post(url,headers=header)
